d9890ec42eebb596eeab6a0583670b943c16bad1
Thiago Dias <thiago@thir.info>
2024-12-08 19:54:07 -0300

    feat: add setup.py to configure project installation
    Introduce setup.py to automate package installation by parsing details from pyproject.toml. This will ensure that project dependencies and metadata are correctly handled during the setup process.

---
+++ setup.py
@@ -0,0 +1,39 @@
+"""Setup for LogFusion."""
+
+import tomllib
+from pathlib import Path
+from typing import Any
+
+from setuptools import find_packages, setup
+
+
+def load_pyproject_toml(file_path: Path) -> dict[str, Any]:
+    """Load the pyproject.toml file and return its contents as a dictionary."""
+    with file_path.open("rb") as f:
+        return tomllib.load(f)
+
+
+def main() -> None:
+    """Main method."""
+    # Load the pyproject.toml file
+    pyproject_content = load_pyproject_toml(Path("pyproject.toml"))
+
+    # Extract necessary information from the pyproject.toml file
+    project_info = pyproject_content["project"]
+
+    setup(
+        name=project_info["name"],
+        version=project_info["version"],
+        description=project_info["description"],
+        author=project_info["authors"][0],
+        author_email=project_info["authors"][0],
+        url=project_info.get("url", "https://github.com/Thealdersonproject/LogFusion"),
+        packages=find_packages(),
+        install_requires=project_info["dependencies"],
+        classifiers=project_info["classifiers"],
+        python_requires=project_info["requires-python"],
+    )
+
+
+if __name__ == "__main__":
+    main()
