04cc1232446b3bda1e42cd346753f709b611a29a
Thiago Dias <thiago@thir.info>
2024-12-08 20:09:09 -0300

    feat: enhance module initialization with imports
    Added key components to the __init__.py for streamlined access, including configuration handling and logging functionalities. This change improves the usability and readability of the logfusion module by making essential functions and classes readily available.

--- logfusion/__init__.py
+++ logfusion/__init__.py
@@ -1,1 +1,7 @@
 """This is the __init__.py file.txt for the logfusion files."""
+
+from .config import default_config, get_config, set_config
+from .decorators import log_function, log_method
+from .logger import Logger
+
+__all__ = ["Logger", "default_config", "get_config", "log_function", "log_method", "set_config"]
