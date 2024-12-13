b2fd17dfa461b8f7f144ac0988861299c898677c
Thiago Dias <thiago@thir.info>
2024-12-13 10:29:10 -0300

    refactor(setup.py): enhance packaging and metadata
    Updated `setup.py` to include long description, improve classifiers, and align project metadata with modern packaging standards. Removed unused imports and restructured package data handling to include JSON and YAML files. Enhanced clarity and functionality for package distribution.

--- setup.py
+++ setup.py
@@ -5,13 +5,17 @@ import tomllib
 from pathlib import Path
 from typing import Any
 
-from setuptools import find_packages, setup
+from setuptools import setup
 
 
 def load_pyproject_toml(file_path: Path) -> dict[str, Any]:
     """Load the pyproject.toml file and return its contents as a dictionary."""
     with file_path.open("rb") as f:
         return tomllib.load(f)
+
+
+with Path("README.md").open(encoding="utf-8") as fh:
+    long_description = fh.read()
 
 
 def main() -> None:
@@ -23,18 +27,38 @@ def main() -> None:
     project_info = pyproject_content["project"]
 
     setup(
+        # Basic project information
         name=project_info["name"],
         version=project_info["version"],
-        description=project_info["description"],
         author=project_info["authors"][0],
         author_email=project_info["authors"][0],
-        url=project_info.get("url", "https://github.com/Thealdersonproject/LogFusion"),
-        packages=find_packages(),
-        install_requires=project_info["dependencies"],
-        classifiers=project_info["classifiers"],
-        python_requires=project_info["requires-python"],
+        description=project_info["description"],
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        # Project URLs
+        url="https://github.com/TheAldersonProject/YapLogger",
+        # Package discovery
+        package_dir={"yaplogger": ""},
+        # Package data
         include_package_data=True,
-        package_data={"typings": ["*.pyi"]},
+        package_data={
+            "": ["*.json", "*.yaml", "*.pyi"],
+        },
+        # Dependencies
+        python_requires=project_info["requires-python"],
+        install_requires=project_info["dependencies"],
+        # Classifiers help users find your project
+        classifiers=[
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: BSD License",
+            "Operating System :: OS Independent",
+            f"Programming Language :: Python :: {project_info['requires-python']}",
+            "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+        # Additional metadata
+        platforms=["any"],
+        zip_safe=False,
     )
 
 
