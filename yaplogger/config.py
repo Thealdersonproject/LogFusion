665011b1c737ed6c1b1b0484a23ac21317ddb546
Thiago Dias <thiago@thir.info>
2024-12-13 15:32:50 -0300

    docs: add detailed module docstring to config.py
    Enhances the docstring for the config module with detailed usage instructions, descriptions of functions, and example code. This improves clarity and usability for developers working with logging configuration.

--- yaplogger/config.py
+++ yaplogger/config.py
@@ -1,4 +1,28 @@
-"""Load the configuration from pyproject.toml."""
+# ./yaplogger/config.py
+"""Load the configuration from pyproject.toml.
+
+This module provides functions to load, set, and get the logging configuration for the YapLogger project.
+The configuration is stored in a global variable and can be modified using the `set_config` function. The `get_config`
+function retrieves the current configuration, taking into account any environment variables that might
+override the default settings.
+
+Functions:
+    - set_config: Sets the logging configuration.
+    - get_config: Retrieves the logging configuration.
+
+Usage:
+    ```python
+    from yaplogger.config import set_config, get_config
+
+    # Set a new configuration
+    new_config = {"log_level": "INFO", "dev_mode": True, "handlers": {"loguru": {}}}
+    set_config(new_config)
+
+    # Get the current configuration
+    current_config = get_config()
+    print(current_config)
+    ```
+"""
 
 import os
 from typing import Any
