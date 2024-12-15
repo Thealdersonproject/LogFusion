1925d359bdd3780b7c5c19f499e8f18f8a1471bf
Thiago Dias <thiago@thir.info>
2024-12-15 15:30:17 -0300

    feat: add initial YapLogger package with placeholders
    Initializes the YapLogger project package with an `__init__.py` file. Sets up imports for `Config` and `Constants` to establish a basic structure. This serves as the foundation for future implementation.

---
+++ yaplogger/__init__.py
@@ -0,0 +1,8 @@
+# ./yaplogger/__init__.py
+"""This is the YapLogger project package."""
+
+# implement me.
+from .config import Config
+from .constants import Constants
+
+__all__ = ["Config", "Constants"]
