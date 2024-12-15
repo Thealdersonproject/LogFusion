8be84d2d1cfb1c7307e94166b27d558cd25ff161
Thiago Dias <thiago@thir.info>
2024-12-15 20:01:22 -0300

    refactor: clean up yaplogger module header
    Reformatted the module header by removing unnecessary comments and adding a descriptive marker. This improves readability and aligns with project conventions.

--- yaplogger/__init__.py
+++ yaplogger/__init__.py
@@ -1,7 +1,6 @@
-# ./yaplogger/__init__.py
+# whoami::./yaplogger/__init__.py
 """This is the YapLogger project package."""
 
-# implement me.
 from .config import Config
 from .constants import Constants
 from .log import Log
