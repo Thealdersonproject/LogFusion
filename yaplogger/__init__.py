fbe5ba87a62e0ed5f094a3927f177642f2e723ba
Thiago Dias <thiago@thir.info>
2024-12-15 20:30:22 -0300

    refactor: remove unused imports from yaplogger module
    Cleaned up unused imports `Config` and `Constants` from the `__init__.py` file to streamline the module. Updated the `__all__` list to include only `Log`, which is the sole usable component.

--- yaplogger/__init__.py
+++ yaplogger/__init__.py
@@ -1,8 +1,6 @@
 # whoami::./yaplogger/__init__.py
 """This is the YapLogger project package."""
 
-from .config import Config
-from .constants import Constants
 from .log import Log
 
-__all__ = ["Config", "Constants", "Log"]
+__all__ = ["Log"]
